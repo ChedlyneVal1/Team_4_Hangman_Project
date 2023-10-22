53fb6c2b22db65baddbc0562ff0ee8b8564787c5
jrknight <jknight1694@gmail.com>
2023-10-22 16:14:43 -0500

    Updated WordGeneration to Singleton
    Uses singleton to only extract words from file once.

--- src/WordGeneration.java
+++ src/WordGeneration.java
@@ -6,17 +6,40 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class WordGeneration {
-	
-	public String genaratedWord() throws FileNotFoundException
-	{
-		Scanner scanner = new Scanner(new File(".\\Team_4_Hangman_Project\\src\\Words.txt"));
+	// Singleton variable
+    private static WordGeneration instance = null; 
+	private List<String> words = new ArrayList<>();
+    
+    private WordGeneration()
+    {
+    	try {
+    		this.init();
+    	} catch (FileNotFoundException ex) {
+    		// Handle the exception
+    	}
+    }
+    
+    private void init() throws FileNotFoundException
+    {
+    	Scanner scanner = new Scanner(new File(".\\Team_4_Hangman_Project\\src\\Words.txt"));
 		
-		List<String> words = new ArrayList<>();
 		while (scanner.hasNext())
 		{
 			words.add((scanner.nextLine()));
 		}
-		
+    }
+    
+    // Call to get the instance of the singleton WordGeneration
+    public static synchronized WordGeneration getInstance() 
+    { 
+        if (instance == null) 
+        	instance = new WordGeneration(); 
+  
+        return instance; 
+    } 
+    
+	public String genaratedWord() 
+	{	
 		Random random = new Random();
 		
 		String word = words.get(random.nextInt(words.size()));
